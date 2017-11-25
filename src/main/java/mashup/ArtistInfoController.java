c7c54e8ae143247c6e3f28271ada0c41a0e46696
Annci <anen00-git@yahoo.se>
2017-11-25 17:52:21 +0100

    Made parallel with stream

--- src/main/java/mashup/ArtistInfoController.java
+++ src/main/java/mashup/ArtistInfoController.java
@@ -3,9 +3,11 @@ package mashup;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +24,7 @@ public class ArtistInfoController {
 	
     private static final Logger log = LoggerFactory.getLogger(ArtistInfoController.class);
 	
+    @CrossOrigin
 	@RequestMapping("/artist")
 	public ArtistInfo artist(@RequestParam(value="mbid", defaultValue="5b11f4ce-a62d-471e-81fc-a69a8278c7da") String mbid) {
         log.info("mbid = " + mbid);
@@ -41,22 +44,37 @@ public class ArtistInfoController {
 	 * @return list of album cover image urls
 	 */
 	public List<Album> getAlbumList(List<ReleaseGroup> releaseGroups) {
-		List<Album> albumList = new ArrayList<Album>();
+		List<Album> albumList;// = new ArrayList<Album>();
+		Long startTime = System.currentTimeMillis();
+		
+		albumList = releaseGroups.parallelStream()
+				.map(releaseGroup->getAlbum(releaseGroup))
+				.collect(Collectors.toList());
+		
+        Long stopTime = System.currentTimeMillis();
+        log.info("Time: " + (stopTime-startTime));
+		return albumList;
+	}
+	
+	/**
+	 * 
+	 * @param releaseGroup
+	 * @return
+	 */
+	private Album getAlbum(ReleaseGroup releaseGroup) {
         RestTemplate restTemplate = new RestTemplate();
         CoverImage coverImage;
         String image = "";
-        for (ReleaseGroup releaseGroup : releaseGroups) {
-        	
-        	try {
-        		coverImage = restTemplate.getForObject("http://coverartarchive.org/release-group/" + releaseGroup.getId(), CoverImage.class);
-        		image = coverImage.getImages().get(0).getImage();
-        	}
-        	catch (HttpClientErrorException he) {
-        		image = "";
-        	}
-			albumList.add(new Album(releaseGroup.getTitle(), releaseGroup.getId(), image));
-		}
-		return albumList;
+        
+    	try {
+    		coverImage = restTemplate.getForObject("http://coverartarchive.org/release-group/" + releaseGroup.getId(), CoverImage.class);
+    		image = coverImage.getImages().get(0).getImage();
+    	}
+    	catch (HttpClientErrorException he) {
+    		image = "";
+    	}
+    	
+    	return new Album(releaseGroup.getTitle(), releaseGroup.getId(), image);
 	}
 
 }
